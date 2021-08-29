1fa267e0d2d27ed451ac1d8df1d615f1feaf92ad
chaitanya005 <muvvalachaitanya05@gmail.com>
2021-08-29 20:01:43 +0530

    randome

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.get("/helo", (req, res) => {
+  res.send({ message: "hey! there" });
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
